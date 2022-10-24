6eb6fe85cc8b9c108eabc264000ab5f890643023
Nathan Jodoin <nathanjodoin@myunt.edu>
2022-10-24 13:35:08 -0500

    Defined unpopulated zone class definitions.

--- unpop_zones.cpp
+++ unpop_zones.cpp
@@ -1,9 +1,49 @@
-// all the class definitions for unpopulated zones in this file
+//Author: Nathan Jodoin
+//CSCE2110 SimCity
+//Recitation Sec. 213 Group 6
+//Unpopulated zone class definitions.
+#include <zone.cpp>
 
 //road class
+class road: public zone
+{
+  private:
+    //nobody here but us chickens
+  public:
+    road(){
+      //nothing happens here
+    }
+};
 
 //powerline class
+class powerline: public zone
+{
+  private:
+    //nobody here buy us chickens
+  public:
+    powerline(){
+      //nothing happens here
+    }
+};
 
 //powered_road class
+class powered_road: public zone
+{
+  private:
+    //nobody here but us chickens
+  public:
+    powered_road(){
+      //nothing happens here
+    }
+};
 
 //powerplant class
+class powerplant: public zone
+{
+  private:
+    //nobody here but us chickens
+  public:
+    powerplant(){
+      //nothing happens here... yet?
+    }
+};

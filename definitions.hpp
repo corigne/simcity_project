1395dfe670b76445757a85a5afb23da7a320cb6f
Nathan Jodoin <nathanjodoin@myunt.edu>
2022-10-25 15:14:59 -0500

    added global type_info constant definitions for typeid() comparisons )

--- definitions.hpp
+++ definitions.hpp
@@ -10,8 +10,10 @@
 #include <vector>
 #include <iostream>
 #include <iomanip>
+#include <typeinfo>
 #include "zone.cpp"
 #include "unpop_zones.cpp"
+#include "adjacencies.cpp"
 
 struct map{
   std::vector< std::vector<zone> > map_grid;
@@ -19,6 +21,25 @@ struct map{
   int y_size;
 };
 
+// TYPEID DEFINITIONS for typeid() comparisons
+const type_info& EMPTY = typeid(zone());
+const type_info& ROAD = typeid(road());
+const type_info& PLINE = typeid(powerline());
+const type_info& PROAD = typeid(powered_road());
+const type_info& PPLANT = typeid(powerplant());
+const type_info& RESIDENTIAL = typeid(residential());
+const type_info& COMMERCIAL = typeid(commercial());
+const type_info& INDUSTRIAL = typeid(industrial());
+
+//HELPER FUNCTION TO CONVERT ZONE TO CHAR
+char zone_to_char(zone zone)
+{
+  /*
+  if(typeid(zone) == zone()){
+    return ' ';
+  }
+  */
+}
 //Leave additional useful definitions here.
 
 #endif

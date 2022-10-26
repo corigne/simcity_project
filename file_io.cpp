5d9c72192ce1b4ab6d2bfdd6ebee24f52e1d31ce
Bridges, Trevonne <trevonnebridges@my.unt.edu>
2022-10-26 21:36:16 +0000

    Update file_io.cpp rough draft need to verify that it is working

--- file_io.cpp
+++ file_io.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-vector fileIO(string config)
+vector fileIO(string config, zone &map)
 {
 	ifstream  in_f;
 	vector<string> data;
@@ -47,5 +47,54 @@ vector fileIO(string config)
 	}
 
 	in_f.close();
+
+	int x, y;
+
+	for(int i = 0; i < city.size(); i++)
+	{
+			for(int j = 0; j < city[i].size; j++)
+			{
+				y = i;
+				x = j;
+				if(city[i][j] == ' ')
+					continue;
+				if(city[i][j] == 'I')
+				{
+					industrial tempi(x, y);
+					map.push_back(tempi);
+				}
+				if(city[i][j] == 'R')
+				{
+					residential tempr(x, y);
+					map.push_back(tempr);
+				}
+				if(city[i][j] == 'C')
+				{
+					commercial tempc(xy);
+					map.push_back(tempc);
+				}
+				if(city[i][j] == 'T')
+				{
+					commercial tempt(xy);
+					map.push_back(tempt);
+				}
+				if(city[i][j] == 'P')
+				{
+					commercial tempc(xy);
+					map.push_back(tempc);
+				}
+				if(city[i][j] == '-')
+				{
+					road tempr(xy);
+					map.push_back(tempr);
+				}
+				if(city[i][j] == '#')
+				{
+					powerplant temppow(xy);
+					map.push_back(temppow);
+				}
+			}
+	}
+
 	return city;
 }

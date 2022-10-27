cf6c6fc96d35a566320ce1692330445b647fe460
Chandler Garrett <chandlergarrett@my.unt.edu>
2022-10-27 10:20:41 -0500

    "Moved industrial growth to update update_map"

--- update_map.cpp
+++ update_map.cpp
@@ -10,4 +10,38 @@
 
 
 //industrial
+void IndustiralGrowth(industrial * industryNodeUpdate){
+int currentPopulation = industryNodeUpdate->getPopulation();
+if(currentPopulation>4)
+currentpopulation =4;
+switch (currentPopulation){
+
+case 0;
+      for(int PGI :  )
+
+break;
+
+  case 1:
+
+
+break;
+
+  case 2:
+
+break;
+
+  case 3:
+
+break;
+
+  case 4:
+
+break;
+
+  default:
+
+cout<<"Industrial growth update error invalid population retrival"<<endl;
+
+break;
+};
 

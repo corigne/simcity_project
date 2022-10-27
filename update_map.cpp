1426f158c81b2f043d5fe4ad79897346129f6167
Nathan Jodoin <nathanjodoin@myunt.edu>
2022-10-27 16:29:31 -0500

    added function definition for populate_zlist to update_map.cpp

--- update_map.cpp
+++ update_map.cpp
@@ -1,3 +1,5 @@
+#include "definitions.hpp"
+
 // update function definitions here
 
 //residential
@@ -8,3 +10,53 @@
 
 //industrial
 
+
+//populates provided list struct with res, ind, and comm nodes
+void populate_zlist(Map &city_map, z_list &m_list)
+{
+  for(std::vector<zone*> row : city_map.map_grid)
+  {
+    for(zone* curr : row)
+    {
+      switch(curr->getType())
+      {
+        case 'R':
+        {
+          residential* temp_r = dynamic_cast<residential*>(curr);
+          m_list.res.push_back(temp_r);
+          break;
+        }
+        case 'I':
+        {
+          industrial* temp_i = dynamic_cast<industrial*>(curr);
+          m_list.ind.push_back(temp_i);
+          break;
+        }
+        
+        case 'C':
+        {
+          commercial* temp_c = dynamic_cast<commercial*>(curr);
+          m_list.com.push_back(temp_c);
+          break;
+        }
+      }
+    }
+  }
+}
+
+//sorts populated z_list struct members by population
+void popsort_zlist(z_list &m_list)
+{
+  
+}
+
+//sorts populated z_list struct members by Y coord
+void ysort_zlist(z_list &m_list)
+{
+
+}
+
+void xsort_zlist(z_list &m_list)
+{
+
+}

cfef1ee19e4cc604ea64aec07a2b2b7f63e5007c
Nathan Jodoin <nathanjodoin@myunt.edu>
2022-10-26 22:56:08 -0500

    started building milestone 1 main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 // main function here
 #include "definitions.hpp"
+#include "file_io.cpp"
+#include "adjacencies.cpp"
+#include "display.cpp"
+
 int main(){
   
+  Map * city_map = new Map;
   //read file + populate map struct
-  
+  fileIO(config, map);
+  city_map->x_size = city_map->map_grid.size();
+  city_map->y_size = city_map->map_grid.at(0).size();
+
   //calculate adjacencies
+  calcLocalAdjacencies(city_map);
+  calcRemoteAdjacencies(city_map);
 
   //calculate power
-
+  for(vector<zone*> row : city_map->map_grid)
+  {
+    for(zone* curr : row)
+    {
+      if(curr->getType() == 'P')
+      {
+        powerplant* pplt_ptr = dynamic_cast<powerplant*>(curr);
+        pplt_ptr->setPoweredState(curr);
+      }
+    }
+  }
   //first time display map
+  display(city_map);
+  //// MILESTONE 1 /////
 
   //start menu
 

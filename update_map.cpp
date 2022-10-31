8a3d39d78b3d0063deb21646003224db8db0b704
Chandler Garrett <chandlergarrett@my.unt.edu>
2022-10-30 19:18:25 -0500

    trying to pull sprint2

--- update_map.cpp
+++ update_map.cpp
@@ -35,6 +35,10 @@ break;
 
 case 1:
 for(zone * RGI :residentialZone->getLocallyAdjacent() )
+{
+
+
+}
 
 
 
@@ -42,57 +46,10 @@ for(zone * RGI :residentialZone->getLocallyAdjacent() )
 break;
 
 };
-
-
-for(zone * RGI: residentialZone->getLocallyAdjacent() )
-      {
-         if (*RGI.getPopulation() == 
-
-
-
-      }
-
-
-}
 
 //commercial
 
 
 //industrial
-void IndustiralGrowth(industrial * industryNodeUpdate){
-int currentPopulation = industryNodeUpdate->getPopulation();
-if(currentPopulation>4)
-currentPopulation =4;
 
-
-switch (currentPopulation){
-
-case 0:
-      for(zone* PGI : industryNodeUpdate->getLocallyAdjacent() )
-
-break;
-
-  case 1:
-
-
-break;
-
-  case 2:
-
-break;
-
-  case 3:
-
-break;
-
-  case 4:
-
-break;
-
-  default:
-
-std::cout<<"Industrial growth update error invalid population retrival"<<std::endl;
-
-break;
-};
 

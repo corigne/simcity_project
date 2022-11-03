2296956cddfa1cea90cda5a2af4d171a434cc1d4
Nathan Jodoin <nathanjodoin@my.unt.edu>
2022-11-03 18:30:56 -0500

    Removed debug code, fixed whitespace, added and cleaned up comments for prod.

--- main.cpp
+++ main.cpp
@@ -2,22 +2,27 @@
 // CSCE2110
 // Main function and program loop.
 // CURRENTLY MILESTONE 1
+// definitions.hpp are object and class definitions
 #include "definitions.hpp"
-#include "file_io.cpp"
-#include "adjacencies.cpp"
-#include "display.cpp"
-#include "update_map.cpp"
-#include "analysis.cpp"
+// the rest are functional definitions
+#include "file_io.cpp" //author: Trevonne
+#include "adjacencies.cpp" //author: Nathan
+#include "display.cpp" //author: Salma
+#include "update_map.cpp" //author: Chandler
+#include "analysis.cpp" //author: Salma, Trevonne
 
 int main(int argc, char *argv[]){
   
+  //checks to make sure the user passed a filename argument
   if(argc < 2){
     std::cout << "Please specify an input filename as an argument." << std::endl;
     std::cout << std::endl;
     return 1;
   }
 
+  //the Map is one of two primary data structures, it contains the 2D vector
   Map * city_map = new Map;
+
   //read file + populate map struct
   //CHANGE THIS BACK TO argv[1] before submission this is for debugging
   std::string config = argv[1];
@@ -25,7 +30,7 @@ int main(int argc, char *argv[]){
   city_map->y_size = city_map->map_grid.size();
   city_map->x_size = city_map->map_grid.at(0).size();
 
-  //calculate adjacencies by Jodoin, NJ
+  //calculate adjacencies
   calcLocalAdjacencies(city_map);
   calcRemoteAdjacencies(city_map);
 
@@ -34,6 +39,7 @@ int main(int argc, char *argv[]){
   {
     for(zone* curr : row)
     {
+      //check current type is powerplant, otherwise, nothing needs to be done
       if(curr->getType() == 'P')
       {
         //dynamic casting allows you to call powerplant child functions
@@ -41,7 +47,7 @@ int main(int argc, char *argv[]){
 
         if (pplt_ptr != nullptr)
         {
-          //set up the visited list
+          //set up the visited/discovery list
           std::vector<bool> vis_temp (city_map->x_size, false);
           std::vector< std::vector<bool> > visited(city_map->y_size, vis_temp);
 
@@ -52,18 +58,16 @@ int main(int argc, char *argv[]){
       }
     }
   }
-  // The zlist is a struct with a list for residential, industrial, and commercial nodes each.
-  // This is the working datastructure for the bulk of the work, such as population updates.
+  // The zlist is a struct with a working list of resid. , indus., and comm. zones.
+  // This is the second primary datastructure primarily for population growth and pollution.
   z_list master_list;
   
   //populate the zlist from the current map
   populate_zlist(*city_map, master_list);
+
   //FIRST LIST SORT
+  //lists are sorted by population, then adj population, then lowest Y, then lowest X
   pop_zone_sort(master_list);
-
-
-  //start menu
-  //Display simulation settings and prompt user for simulation start.
 
   // Welcome/credit banner:
   std::cout << std::endl
@@ -72,50 +76,19 @@ int main(int argc, char *argv[]){
   << "~~~ Designed by Group6 ~~~" << std::endl
   << "~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl << std::endl;
 
+  //Display simulation settings
   std::cout << std::endl << "==== Simulation Settings ====" << std::endl
     << "Refresh Rate: every " << city_map->refresh_rate << " steps"<< std::endl
     << "Max Steps   : " << city_map->max_time << " steps" << std::endl << std::endl;
   
-  /* Temporarily Commented Out Until We Consult the TA
-  char user_in = 0;
-  bool validated = false;
-
-  while(!validated)
-  {
-    // Simulation Settings Confirmation
- 
-    std::cout << "Start simulation? [(y)es/(c)ancel]: ";
-
-    //user prompt
-    
-    std::cin >> user_in;
-    //validation
-    if(std::cin.fail())
-    {
-      //clears the input error
-      std::cin.clear();
-      //skips the offending buffer entry for 1 char
-      std::cin.ignore();
-
-    }else if(user_in == 'y' || user_in == 'c'){
-      //continue simulation
-      validated = true;
-    }
-  }
-  //if simulation was cancelled, return from main with 0, intended
-  if(user_in == 'c')
-  {
-    return 0;
-  }
-*/
-
-  //display & update loop
+  //Display & Update loop
   int curr_frame = 0;
   bool change = false;
 
+  //primary exit condition for the loop is if current frame == maxtime
   while (curr_frame <= city_map->max_time)
   { 
-    //if (refresh divisible) then: display post-update
+    //if curr frame is refresh divisible then display the map
     if(curr_frame % city_map->refresh_rate == 0)
     {
       std::cout << "Current Time Step: " << curr_frame << std::endl;
@@ -123,11 +96,10 @@ int main(int argc, char *argv[]){
       std::cout << std::endl;
     }
     
-    //updatemap should return true if nothing was updated between 2 timesteps
-    // else it should return false
+    // updatemap returns true if the map was updated between 2 timesteps
     change = update_map(master_list);
 
-    //pollution updates
+    //pollution updates, recursively, originates from ind nodes
     for(populated* curr : master_list.ind)
     {
       std::list<zone*> q;
@@ -141,19 +113,21 @@ int main(int argc, char *argv[]){
       temp_ind->updatePollution(temp_ind, q, disc);
     }
 
-    //update frame
+    //update frame counter
     curr_frame += 1; 
     
-    //if no updates in from last to current time step, break display/update loop...
+    //if no updates from last to current time step, break display/update loop...
     if(change == false)
     {
       break;
     }    
   }
-  //...display final pollution values
+  
+  // display final pollution values
   displayPollutedMap(city_map, 0, 0, city_map->x_size-1, city_map->y_size-1);
   std::cout << std::endl;
-  //...and begin analysis
+
+  // begin analysis function lopp
   analysis(city_map, master_list);
 
   return 0;

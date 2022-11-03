c90992d05b5c941f9e832ff9cd480bcc50516d5b
Nathan Jodoin <nathanjodoin@my.unt.edu>
2022-11-03 18:37:15 -0500

    Comments added to definitions.hpp

--- definitions.hpp
+++ definitions.hpp
@@ -1,7 +1,7 @@
 //Author: Nathan Jodoin
 //CSCE2110 - SimCity
 //Recitation Section 213 - Group 6
-//General definitions to in included almost everywhere.
+//General definitions the need to be included almost everywhere.
 
 #ifndef _DEFINITIONS_HPP_
 #define _DEFINITIONS_HPP_
@@ -15,35 +15,40 @@
 
 // class includes here
 // avoid functional includes here
-#include "zone.cpp"
-#include "populated.cpp"
-#include "unpop_zones.cpp"
-#include "residential.cpp"
-#include "industrial.cpp"
-#include "commercial.cpp"
+#include "zone.cpp" // Author: Nathan 
+#include "populated.cpp" // Author: Nathan 
+#include "unpop_zones.cpp" // Author: Nathan 
+#include "residential.cpp" // Author: Nathan 
+#include "industrial.cpp" // Author: Nathan 
+#include "commercial.cpp" // Author: Nathan 
 
-
+// type definition for the Map type, the primary working datastructure
 typedef struct Map
 {
+  // a 2D vector of zone* which represents the map
   std::vector< std::vector<zone*> > map_grid;
+  // X and Y size of the map, useful for other functions, primarily bounding
   int x_size;
   int y_size;
+  // data members for the simulation settings, read in from the config file
   int refresh_rate;
   int max_time;
 } Map;
 
+// type def for the second primary data structure, the zlist is a struct of 3
+// populated* vectors representing each of the class types which needs to be check for 
+// population update conditions in an ordered manner, these are sorted
 typedef struct z_list
 {
-  //residential list
+  //residential zone list
   std::vector<populated *> res;
 
-  //industrial list
+  //industrial zone list
   std::vector<populated *> ind;
 
-  //commerical list
+  //commerical zone list
   std::vector<populated*> com;
   
 } z_list;
 
-//Leave additional useful definitions here.
 #endif

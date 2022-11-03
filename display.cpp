a530054e554ba3eae693a203ea0f79324964ffad
Salma <salmaomary@my.unt.edu>
2022-11-03 10:02:56 -0500

    updated display.cpp to display the pollution map

--- display.cpp
+++ display.cpp
@@ -3,6 +3,7 @@
 
 #include "definitions.hpp"
 
+
 //update after milestone 1 with switch case statements for outputting any possible populated zones
 //above update complete 
 
@@ -36,4 +37,23 @@ void displayMap(Map * map)
         }
         std::cout << std::endl;
     }
+}
+
+void displayPollutedMap(Map * map)//function same as above but displays pollution
+{
+    std::cout << std::endl;
+     std::cout << "POLLUTION MAP: "<< std::endl;
+    //X Y coordinates initialized to X=0 and Y=0
+    std::vector<std::vector<zone*> > temp = map->map_grid;
+    for(int y = 0; y < map->y_size; y++)
+    {
+        
+        for(int x = 0; x < map->x_size; x++){
+            zone* tempZone = temp.at(y).at(x);
+            int pollution = tempZone->getPollution();
+            std::cout << " " << pollution << " ";
+        }
+    std::cout << std::endl;
+    }
+    std::cout << std::endl;
 }

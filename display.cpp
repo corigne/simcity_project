4d276cf6502aa16f2a43c90cf16a7328cb25e4cc
Salma <salmaomary@my.unt.edu>
2022-11-03 11:36:52 -0500

    finalized functions with comments

--- display.cpp
+++ display.cpp
@@ -1,23 +1,19 @@
 //Salma Omary
-//Outputs the map according to population
+//Outputs the map according to population, also outputs a map based on the pollution in the zone
 
 #include "definitions.hpp"
 
 
-//update after milestone 1 with switch case statements for outputting any possible populated zones
-//above update complete 
 
 void displayMap(Map * map, int x_1, int y_1, int x_2, int y_2)
 {
-    //X Y coordinates initialized to X=0 and Y=0
-    std::vector<std::vector<zone*> > temp = map->map_grid;
-    for(int y = y_1; y <= y_2; y++)
+    std::vector<std::vector<zone*> > temp = map->map_grid;//initializes temp variable representing zones
+    for(int y = y_1; y <= y_2; y++)//iterates through the map, top to bottom
     {
-        
-        for(int x = x_1; x <= x_2; x++){
+        for(int x = x_1; x <= x_2; x++){//nested for loop that iterates through the map, left to right
             zone* tempZone = temp.at(y).at(x);
             switch(tempZone->getType()){
-                case 'R':case 'C':case 'I':
+                case 'R':case 'C':case 'I'://will output population number rather than zone type for populated zones > 0
                 {
                     populated* curr = dynamic_cast<populated*>(tempZone);
                     int population = curr->getPopulation();
@@ -28,7 +24,7 @@ void displayMap(Map * map, int x_1, int y_1, int x_2, int y_2)
                     }
                     break;
                 }
-                default:
+                default://outputs zone type for non-populated zones
                 {
                     std::cout << " " << tempZone->getType() << " ";
                 }
@@ -39,16 +35,15 @@ void displayMap(Map * map, int x_1, int y_1, int x_2, int y_2)
     }
 }
 
-void displayPollutedMap(Map * map, int x_1, int y_1, int x_2, int y_2)//function same as above but displays pollution
+void displayPollutedMap(Map * map, int x_1, int y_1, int x_2, int y_2)//displays a pollution map
 {
     std::cout << std::endl;
-     std::cout << "POLLUTION MAP: "<< std::endl;
-    //X Y coordinates initialized to X=0 and Y=0
+    std::cout << "POLLUTION MAP: "<< std::endl;
     std::vector<std::vector<zone*> > temp = map->map_grid;
     for(int y = y_1; y <= y_2; y++)
     {
         
-        for(int x = x_1; x <= x_2; x++){
+        for(int x = x_1; x <= x_2; x++){//iterates top left to right and outputs pollution in that zone (including if = 0)
             zone* tempZone = temp.at(y).at(x);
             int pollution = tempZone->getPollution();
             std::cout << " " << pollution << " ";
